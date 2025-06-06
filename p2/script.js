2e4b06d39a41db948602ec15e072dbf7647c1289
GoneVamshi <gonevamshireddy7@gmail.com>
2025-06-06 18:01:16 +0530

    a6 added

--- p2/script.js
+++ p2/script.js
@@ -1,21 +1,19 @@
 const showLogin = () => {
     let str=`
-    <div class="container">
+
         <div class='App-Container'>
             <h1>Login Form</h1>
-            <p><input type="text" class="txtEmail"></p>
-            <p><input type="password" class="txtPass"></p>
+            <p><input type="text" class="txtEmail" required></p>
+            <p><input type="password" class="txtPass" required></p>
             <p><button onclick="showHome()"class="welcome">Log In</button></p>
             <p><button onclick='showRegister()'>Create Account</button></p>
         </div>
-    </div>
     `
     document.querySelector('.root').innerHTML = str
 }
 
 const showRegister = () => {
       let str=`
-      <div class="container">
       <div class="App-Container">
         <h1>Register Form</h1>
         <p><input type="text" class="txtName"></p>
@@ -23,7 +21,6 @@ const showRegister = () => {
         <p><input type="password" class="txtPass"></p>
         <button onclick="showLogin()">Register</button>
         <button onclick="showLogin()">Already have an account</button>
-    </div>
     </div>
     `
     document.querySelector('.root').innerHTML = str
